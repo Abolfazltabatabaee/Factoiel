e80d5bcb0d714c40bd0a55a5143a5b32c0ddc988
AsusIran <AsusIran@AMIRHAMED>
2023-11-01 15:24:15 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int f = 1, num;
+            Console.WriteLine("enter a number");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (int i =1; i <=num;i++)
+            {
+                if (num == 0) break;
+                f *= i;
+            }
+            Console.WriteLine("{0}={1})", num, f);
+                Console.ReadKey();
+        }
+    }
+}
